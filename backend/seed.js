19a0b514dcd60434e7ce04867a4bbb4a8fe96c22
santhoshwesley <santhoshwesleyyer@gmail.com>
2025-02-20 21:19:21 +0530

    mongo connection established

--- backend/seed.js
+++ backend/seed.js
@@ -9,22 +9,22 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("✅ Connected to MongoDB"))
-  .catch((err) => console.error("❌ MongoDB Connection Error:", err));
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB Connection Error:", err));
 
 const doctors = [
   {
-    name: "Dr. John Doe",
+    name: "Dr. Richard",
     specialization: "Cardiologist",
     availableSlots: ["10:00 AM", "11:30 AM", "2:00 PM"],
   },
   {
-    name: "Dr. Sarah Smith",
+    name: "Dr. Muthu",
     specialization: "Dermatologist",
     availableSlots: ["9:00 AM", "12:00 PM", "3:30 PM"],
   },
   {
-    name: "Dr. Raj Patel",
+    name: "Dr. Sravani",
     specialization: "Orthopedic",
     availableSlots: ["10:30 AM", "1:00 PM", "3:00 PM"],
   },
@@ -34,9 +34,9 @@ async function seedDB() {
   try {
     await Doctor.deleteMany({});
     await Doctor.insertMany(doctors);
-    console.log("✅ Doctors Seeded!");
+    console.log("Doctors Seeded!");
   } catch (error) {
-    console.error("❌ Error Seeding Doctors:", error);
+    console.error("Error Seeding Doctors:", error);
   } finally {
     mongoose.connection.close();
   }
